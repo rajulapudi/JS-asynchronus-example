18baac7209e120ea6ef9875ee8bc650af04ebc94
praneeth <rajulapudip@gmail.com>
2019-07-09 13:40:20 +0530

    error mechanism

--- function_callbacks.js
+++ function_callbacks.js
@@ -6,12 +6,13 @@ let url = 'https://jsonplaceholder.typicode.com/users';
 /* Using Functional call backs */
 
 request(url, (err, status, data) => {
-	console.log(data);
+	if (err) {
+		console.log('custom error');
+	} else {
+		console.log(data);
+	}
 });
 
 /* 
-
 here we cannot access the data outside of functional call back
-
-
 */
